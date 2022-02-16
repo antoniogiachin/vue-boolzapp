b39bf8c05ec9bc87a579c6673b6ae97c58e1dcf1
antoniogiachin <giachinantonio@outlook.com>
2022-02-16 11:54:32 +0100

    nuovo approccio visualizzazione chat attiva

--- main.js
+++ main.js
@@ -165,69 +165,61 @@ const app = new Vue (
 
             ],
 
+            // Nuovo testo inserito
             newText: '',
+
+            // Chat attiva
+            active: 0,
 
 
         },
 
         methods : {
 
-            // Funzione cambio visibilita'
-            // Cicla un array di obj e setta la visibilita' a false
-            // Successivamente imposta visibilita' a true per array cliccato
-            changeVisibility(array, index){
-
-                for(let i=0; i < array.length; i++){
-
-                    array[i].visible = false;
-
-                }
-                
-                this.contacts[index].visible = true;
+            // Funzione contatto attivo 
+            setContactActive(index){
+                this.active=index;
             },
-
 
             //Funzione aggiunta messaggio e risposta
-            addMsg(index){
+            sendMessage(){
 
-                // push nuovo messaggio mandato
-                this.contacts[index].messages.push(
-
-                    {
-
-                        date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
-
-                        text: this.newText,
-
-                        status: 'sent'
-
-                    },
-
-
-                );
-                
-                // svuoto input msg
-                this.newText='';
-
-                // Funzione di risposta automatica dopo 1 secondo
-                setTimeout(() => {
-                    this.contacts[index].messages.push(
-            
-                        {
-                
-                            date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
-                
-                            text: 'ok',
-                
-                            status: 'received'
-                
-                        },
-                
-                
-                    );
+                const newMessageObj = {
+     
+                 date: 'vediamo dopo',
+     
+                 text : this.newMessage,
+     
+                 status: 'sent',
+     
+                };
+     
+                this.contact[this.active].messages.push(newMessageObj);
+     
+                this.newMessage = '';
+     
+                const newMessageReply = {
+     
+                 // Prende la data e gli diciamo il formato di uscita che vogliamo
+                 // https:day.js.org/docs/en/display/format
+                 date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
+     
+                 text : 'ok',
+     
+                 status: 'reviced',
+     
+                };
+     
+                // Uso arrow function per avere lo scope uguale, il this altrimenti si riferisce al settimeout e non allo scope di vue
+                setTimeout(() =>{
+     
+                 this.contacts[this.active].message.push(newMessageReply);
+     
                 }, 1000);
-
-            },
+     
+     
+            }, 
+            
 
 
         },
